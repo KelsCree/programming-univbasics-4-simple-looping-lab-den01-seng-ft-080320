750d2bec5649465976c2f9206445fca1307c82aa
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-12 18:38:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
-def loop_message_five_times('Hello World.')
+def loop_message_five_times(string)
   counter = 0
   while counter < 5 do
-  puts 'Hello World'
+  puts string
   counter += 1
 end
 end
